f2bf6fa37cd46c7dc9d5be5ae2e8c51d11d6dc31
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2023-10-04 21:13:56 +0200

    setting up tailwind

--- client/application.ts
+++ client/application.ts
@@ -3,20 +3,26 @@ import "./style.css"
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="flex flex-row justify-center">
+    <a href="https://vitejs.dev" target="_blank" class="text-indigo-500 font-medium hover:text-indigo-900">
+      <img src="http://localhost:5173/vite.svg" class="h-[6rem] p-[1.5rem] hover:drop-shadow-[0_0_2rem_#646cffaa] will-change-[filter] transition-[filter] duration-300" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank" class="text-indigo-500 hover:text-indigo-900">
+      <img src="http://localhost:5173/typescript.svg" class="h-[6rem] p-[1.5rem] hover:drop-shadow-[0_0_2rem_#3178c6aa] will-change-[filter] transition-[filter] duration-300" alt="TypeScript logo" />
+    </a>
+  </div>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="http://localhost:5173/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="http://localhost:5173/typescript.svg" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1 class="text-[3.2rem] leading-[1.1] font-[system-ui]">Vite + TypeScript</h1>
+    <div class="p-[2em]">
+      <button 
+        id="counter"
+        type="button"
+        class="rounded-[8px] border-[1px] border-transparent hover:border-indigo-500 py-[0.6em] px-[1.2em] text-[1em] 
+            font-medium bg-stone-900 cursor-pointer transition-[border-color] duration-[0.25s] focus:focus-visible:outline-4 
+            focus:focus-visible:outline focus:focus-visible:outline[-webkit-focus-ring-color]"
+      ></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <p>Click on the Vite and TypeScript logos to learn more</p>
   </div>
 `
 
